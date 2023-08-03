914898a3bf527b37ed6218b64e8080d323ee8a78
cmclausen <91616647+cmclausen@users.noreply.github.com>
2023-08-03 08:38:37 +0200

    Update setup.py

--- utils/setup.py
+++ utils/setup.py
@@ -5,7 +5,6 @@ version='0.1',
 description='Personal utilities',
 url='#',
 author='Christian M. Clausen',
-#install_requires=['numpy','spicy','itertools','iteround','ase','pickle','torch','torch_geometric','matplotlib','imageio','tqdm',],
 author_email='',
 packages=setuptools.find_packages(),
 zip_safe=False)
